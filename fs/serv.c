dde9071e473024200c99e7569ad2b49865e23fdc
Federico del Mazo <fdelmazo@fi.uba.ar>
2019-06-17 23:14:00 -0300

    Implementa serve_write

--- fs/serv.c
+++ fs/serv.c
@@ -243,6 +243,9 @@ serve_read(envid_t envid, union Fsipc *ipc)
 int
 serve_write(envid_t envid, struct Fsreq_write *req)
 {
+	struct OpenFile *o;
+	int r;
+
 	if (debug)
 		cprintf("serve_write %08x %08x %08x\n",
 		        envid,
@@ -250,7 +253,14 @@ serve_write(envid_t envid, struct Fsreq_write *req)
 		        req->req_n);
 
 	// LAB 5: Your code here.
-	panic("serve_write not implemented");
+	if ((r = openfile_lookup(envid, req->req_fileid, &o)) < 0)
+		return r;
+
+	if ((r = file_write(o->o_file, ret->ret_buf, req->req_n, o->o_fd->fd_offset)) < 0)
+		return r
+
+	o->o_fd->fd_offset += r;
+	return r;
 }
 
 // Stat ipc->stat.req_fileid.  Return the file's struct Stat to the
