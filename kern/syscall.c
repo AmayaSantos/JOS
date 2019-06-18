f99bcad76b81395ec4a4122bb58ef717d23ebcef
Federico del Mazo <fdelmazo@fi.uba.ar>
2019-06-17 23:56:04 -0300

    Arregla TP4-Parte 5

--- kern/syscall.c
+++ kern/syscall.c
@@ -504,7 +504,7 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 			return sys_ipc_recv((void*) a1);
 
 		case SYS_ipc_try_send:
-			return sys_ipc_try_send(a1, a2, (void*) a3, a4);
+			return sys_ipc_try_send((envid_t) a1, (uint32_t) a2, (void *) a3, (unsigned) a4);
 
 		case SYS_env_set_pgfault_upcall:
 			return sys_env_set_pgfault_upcall(a1, (void*) a2);
--- lib/file.c
+++ lib/file.c
@@ -144,16 +144,11 @@ devfile_write(struct Fd *fd, const void *buf, size_t n)
 	int r;
 
 	fsipcbuf.write.req_fileid = fd->fd_file.id;
-	fsipcbuf.write.req_n = n;
-
 	size_t size = sizeof(fsipcbuf.write.req_buf);
-	size_t to_write = size < n ? n : size;
-	memmove(fsipcbuf.write.req_buf, buf, to_write);
+	fsipcbuf.write.req_n = (n > size) ? size : n;
 
-	if ((r = fsipc(FSREQ_WRITE, NULL)) < 0)
-		return r;
-
-	return r;
+	memmove(fsipcbuf.write.req_buf, buf, fsipcbuf.write.req_n);
+	return fsipc(FSREQ_WRITE, NULL);
 }
 
 static int
